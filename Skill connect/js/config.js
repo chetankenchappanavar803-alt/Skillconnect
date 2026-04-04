db2bddf48ae339d8cb998eaff8939b8a4071a4fe
chetankenchappanavar803-alt <chetankenchappanavar803@gmail.com>
2026-04-04 19:50:56 +0530

    Delete Skill connect directory

--- Skill connect/js/config.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL = 'https://pokpvnxikvbhfxpgfces.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBva3B2bnhpa3ZiaGZ4cGdmY2VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzMDM0NzgsImV4cCI6MjA5MDg3OTQ3OH0.V1OjGbnGNwNH0md9oNLPWQPFoNUQFpPfSpngdlzC2TQ';
-
-// Initialize Supabase
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
